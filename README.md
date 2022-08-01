e3970e3fa53aafea309d258c9a5e11eb442cb55d
Romazanov <romazanovma@gmail.com>
2022-08-01 04:36:46 +0400

    Add product parser

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # redesigned-umbrella
+Wildberries category parser
+
+usage: category_parser.py [-h] [--category CATEGORY] [--subcategory SUBCATEGORY]
+
+Parse a wildberries subcategory.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --category CATEGORY   Specify the category
+  --subcategory SUBCATEGORY
+                        Specify the subcategory
+
+usage example: python3 category_parser.py --category "Мужчинам" --subcategory "Верхняя одежда"
--- wbparser/category_parser.py
+++ wbparser/category_parser.py
@@ -5,119 +5,140 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
-from db import *
-
-
-def list_get(list, idx, default):
-    '''safe get method for list'''
-    try:
-        return list[idx]
-    except IndexError:
-        return default
+import argparse
 
 
-DELAY = 3
+DELAY = 3 #timeout in seconds
+WEBSITE = "https://www.wildberries.ru/"
+CATEGORY_MENU = "ul.menu-burger__main-list"
+CATEGORY_LINK = "a.menu-burger__main-list-link"
+SUBCATEGORY_MENU = "a.j-menu-item"
+PRODUCT_CARD = "a.product-card__main"
+PRODUCT_ARTICLE = "p.product-article"
 
 
-# python3 category_parser.py "https://www.wildberries.ru/" "Мужчинам" "Верхняя одежда" 5
 def main(*args):
-    if len(args) >= 4:
-        driver = webdriver.Firefox()
-        db = Db()
-        db.drop()
-        link = link_build(driver, website=list_get(args,1,None),
-            category=list_get(args,2,None), subcategory=list_get(args,3,None),
-            page=list_get(args,4,1))
-        if link == None:
-            print("Category not found")
+    try:
+        driver = None
+        arg_parser = argparse.ArgumentParser(description='Parse a wildberries subcategory.')
+        arg_parser.add_argument('--category', help='Specify the category')
+        arg_parser.add_argument('--subcategory',help='Specify the subcategory')
+        args = vars(arg_parser.parse_args())
+        category = args['category']
+        subcategory = args['subcategory']
+        if category and subcategory:
+            driver = init()
+            subcategory_link = subcategory_parser(driver, category=category,
+            subcategory=subcategory)
+            if subcategory_link:
+                cards = parse_products(driver, subcategory_link)
+                [show_card(card) for card in cards]
+                print(f"number of products parsed:{len(cards)}")
+            else:
+                print("Category not found")
         else:
-            print(link)
-            parsed = parse(driver, link)
-            db.write_db(parsed)
-        show = db.get()
-        for item in show:
-                print(f'{item.id}\t{item.brand}\t{item.card_name}\t{item.name}\t{item.price}\t{item.old_price}\t{item.description}\t{item.rating}\n')
-        driver.close()
-    else:
-        print("usage: python3 category_parser.py website category subcategory page(optional)")
+            arg_parser.print_help()
+    finally:
+        if driver:
+            driver.close()
 
-def link_build(driver, website, category=None, subcategory=None, page=1, delay=DELAY):
+
+def show_card(card):
+    print('{')
+    for key,value in card.items():
+        print(f'{key}:"{value}",\n')
+    print('}\n')
+
+
+def init():
+    return webdriver.Firefox()
+
+
+def subcategory_parser(driver, category, subcategory):
+    driver.get(WEBSITE)
+    WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, CATEGORY_MENU)))#May add retries
+    # category_menu = driver.find_element(By.CSS_SELECTOR, CATEGORY_MENU)
+    # category_url = category_menu.find_element(By.CSS_SELECTOR, f"{CATEGORY_LINK}[text='{category}']")
+    category_menu = driver.find_elements(By.CSS_SELECTOR, CATEGORY_LINK)
+    for category_element in category_menu:
+        if category_element.get_attribute("text")==category:
+            category_link = category_element.get_attribute("href")
+            break #breaks after finds needed category
+
+    driver.get(category_link)
+    WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, SUBCATEGORY_MENU)))
+    subcategory_menu = driver.find_elements(By.CSS_SELECTOR, SUBCATEGORY_MENU)#attribute selector instead of cycle
+    for subcategory_element in subcategory_menu:
+        if subcategory_element.get_attribute("text")==subcategory:
+            subcategory_link = subcategory_element.get_attribute("href")
+            break #breaks after finds needed subcategory
+    return subcategory_link
+
+
+def parse_products(driver, subcategory_link):
+    driver.get(subcategory_link)
+    WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, PRODUCT_CARD)))
+    product_list = driver.find_elements(By.CSS_SELECTOR, PRODUCT_CARD)
+    card_list = [parse_product_card(product) for product in product_list]
+    return [parse_product_page(driver, card) for card in card_list]
+
+
+def parse_product_page(driver, card):
+    driver.get(card["url"])
     try:
-        driver.get(website)
-        try:
-            WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li.menu-burger__main-list-item")))
-        except TimeoutException:
-            print("Error: get website page Timeout")
-        menu = driver.find_elements(By.CSS_SELECTOR, "li.menu-burger__main-list-item")
-        link = None
-        if category:
-            for elem in menu:
-                link_elem = elem.find_element(By.CSS_SELECTOR, "a.menu-burger__main-list-link")
-                if link_elem.get_attribute("text")==category:
-                    link = link_elem.get_attribute("href")
-            if subcategory:
-                driver.get(link)
-                try:
-                    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.j-menu-item")))
-                except TimeoutException:
-                    print("Error: get category page Timeout")
-                menu = driver.find_elements(By.CSS_SELECTOR, "a.j-menu-item")
-                for elem in menu:
-                    if elem.get_attribute("text")==subcategory:
-                        link = elem.get_attribute("href")
-            link+=f"?page={page}"
-        return link
-
-    except Exception as e:
-        print(f"Exception in link_build:\n{e}")
-
-def parse(driver, link):
-    driver.get(link)
-    try:
-        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.product-card__main")))
+        WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, PRODUCT_ARTICLE)))
     except TimeoutException:
-        print("Error: get subcategory page Timeout")
-    catalog = driver.find_elements(By.CSS_SELECTOR, "a.product-card__main")
-    card_list = []
-    for product in catalog:
-        product_link = product.get_attribute("href")
-        brand = product.find_element(By.CSS_SELECTOR, "strong.brand-name").text
-        card_name = product.find_element(By.CSS_SELECTOR, "span.goods-name").text
-        price = product.find_element(By.CLASS_NAME, "lower-price").text
-        try:
-            old_price = product.find_element(By.CLASS_NAME, "price-old-block").text
-        except Exception:
-            old_price = None
-        driver.get(product_link)
-        try:
-            WebDriverWait(driver, DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.product-article")))
-        except TimeoutException:
-            print("Error: get product page Timeout")
-        id = driver.find_element(By.CSS_SELECTOR, "p.product-article").text.replace("Арт: ", "").replace("Артикул: ", "")
-        name = driver.find_element(By.CSS_SELECTOR, "h1").text
-        description = driver.find_element(By.CSS_SELECTOR, "p.collapsable__text").text
-        try:
-            rating = driver.find_element(By.CSS_SELECTOR, "span.address-rate-mini").text
-        except Exception:
-            rating = None
-        card = {
-            "id": id,
-            "brand": brand,
-            "card_name": card_name,
-            "name": name,
-            "price": price,
-            "old_price": old_price,
-            "description": description,
-            "rating": rating
-        }
-        card_list.append(card)
-        driver.back()
-        try:
-            WebDriverWait(driver, DELAY).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.pagination-item")))
-        except TimeoutException:
-            print("Error: driver.back() Timeout")
-    return card_list
+        print("Error: Product page Timeout")
+    id = find_element_text_safe(driver, By.CSS_SELECTOR, PRODUCT_ARTICLE)
+    if id:
+        id = id.replace("Арт: ", "").replace("Артикул: ", "")
+    name = find_element_text_safe(driver, By.CSS_SELECTOR, "h1")
+    description = find_element_text_safe(driver, By.CSS_SELECTOR, "p.collapsable__text")
+    rating = find_element_text_safe(driver, By.CSS_SELECTOR, "span.address-rate-mini")
+    return {
+        "id": id,
+        "brand": card["brand"],
+        "card_name": card["card_name"],
+        "name": name,
+        "price": card["price"],
+        "old_price": card["old_price"],
+        "description": description,
+        "rating": rating,
+        "url": card["url"],
+    }
+
+
+def parse_product_card(product):
+    product_url = product.get_attribute("href")
+    brand = find_element_text_safe(product, By.CSS_SELECTOR, "strong.brand-name")
+    card_name = find_element_text_safe(product, By.CSS_SELECTOR, "span.goods-name")
+    price = find_element_text_safe(product, By.CLASS_NAME, "lower-price")
+    old_price = find_element_text_safe(product, By.CLASS_NAME, "price-old-block")
+    return {
+        "brand": brand,
+        "card_name": card_name,
+        "price": price,
+        "old_price": old_price,
+        "url": product_url,
+    }
+
+
+def find_element_text_safe(source, selector, key):
+    try:
+        element = source.find_element(selector, key).text
+    except NoSuchElementException:
+        element = None
+    return element
+
+
+def find_element_safe(source, selector, key):
+    try:
+        element = source.find_element(selector, key)
+    except NoSuchElementException:
+        element = None
+    return element
 
 
 if __name__ == "__main__":
