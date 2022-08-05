0187c41b3a8288926ae2c514d0e41b861e22e87e
Romazanov <romazanovma@gmail.com>
2022-08-05 04:37:35 +0400

    add scrapy variant

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ optional arguments:
                         Specify the subcategory
 
 usage example: python3 category_parser.py --category "Мужчинам" --subcategory "Верхняя одежда"
+
+# scrapy
+usage: scrapy crawl -a category=CATEGORY -a subcategory=SUBCATEGORY -a page=PAGE(optional) wb
+
+usage example: scrapy crawl -a category="Мужчинам" -a subcategory="Джинсы" -a page=1 wb
+
---
+++ scrapy/wbparser/spiders/category_parser.py
@@ -0,0 +1,65 @@
+import functools
+import json
+
+import scrapy
+
+
+class MySpider(scrapy.Spider):
+    name = 'wb'
+    allowed_domains = ['wildberries.ru/']
+
+    def __init__(self, category, subcategory, page=1, name=None, **kwargs):
+        if category and subcategory:
+            self.category = category
+            self.subcategory = subcategory
+            self.page = page
+            super().__init__(name, **kwargs)
+        else:
+            self.log(f"usage: scrapy crawl -a category=CATEGORY -a subcategory=SUBCATEGORY -a page=PAGE(optional) wb")
+            exit()
+
+    def start_requests(self):
+        url = "https://www.wildberries.ru/webapi/menu/main-menu-ru-ru.json"
+        yield scrapy.Request(url, callback=self.get_query)
+
+    def get_query(self, response):
+        category_object = [category for category in json.loads(response.body) if category["name"]==self.category][0]
+        subcategory_object = [subcategory for subcategory in category_object["childs"] if subcategory["name"]==self.subcategory][0]
+        subcategory_query = subcategory_object["query"]
+        self.log(f'Got query for subcategory: {subcategory_query}')
+        url = f"https://catalog.wb.ru/catalog/men_clothes/catalog?curr=rub&lang=ru&locale=ru&{subcategory_query}&page={self.page}"
+        # for category in json.loads(response.body):
+        #     if category["name"] == category_name:
+        #         for subcategory in category["childs"]:
+        #             if subcategory["name"] == subcategory_name:
+        #                 subcategory_query = subcategory["query"]
+        #                 self.log(f'Got query for subcategory: {subcategory_query}')
+        #                 break
+        #         break
+        yield scrapy.Request(url, callback=self.parse_category,dont_filter=True)
+
+    def parse_category(self, response):
+        products_list = json.loads(response.body)["data"]["products"]
+        url = f"https://card.wb.ru/cards/detail?locale=ru&lang=ru&curr=rub&dest=-1075831&nm="
+        for product in products_list:
+            url += f'{product["id"]};'
+        yield scrapy.Request(url, callback=self.parse_card,dont_filter=True)
+
+    def parse_card(self, response):
+        cards_list = json.loads(response.body)["data"]["products"]
+        with open("cards_snapshot.result", 'w') as f:
+            for card in cards_list:
+                parsed_card = {
+                    "id": card["id"],
+                    "name": card["name"],
+                    "brand": card["brand"],
+                    "price": card["salePriceU"],
+                    "old_price": card["priceU"],
+                    "rating": card["rating"],
+                    "feedbacks": card["feedbacks"],
+                    "sizes": [
+                        {"name": size["name"], "count": functools.reduce(lambda x,y: x+y['qty'],size["stocks"],0)} for size in card["sizes"]
+                        ],
+                    # "balance": size["stocks"]
+                }
+                f.write(f'{parsed_card}\n')
